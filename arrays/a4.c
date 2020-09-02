0a3b54be1e7ebce2272fdeae0a04bb784fded62d
kunasuman <kunasuman666@gmail.com>
2020-09-02 23:02:26 +0530

    add arr 4 print fuc

--- arrays/a4.c
+++ arrays/a4.c
@@ -3,7 +3,8 @@ void main()
 {
 
 	int i,j;
-	int a[5],b[5];
+	int a[5];
+	int temp;
 
 	printf("enter the values arays \n");
 	i=0;
@@ -13,15 +14,26 @@ void main()
 		i++;
 	}
 
+	i=0;
+	j=4;	
+	// 5 7 13 6 2 
+	while(i < j)
+	{
+		temp=a[i];
+		a[i]=a[j];
+		a[j]=temp;
+	//	printf("%d ",temp);
+		i++;
+		j--;
+	}
 
-
-	printf("rvrs ary values: ");
-
-	i=4;
-	while(i >= 0)
+	printf("rvsr elemnts\n");
+	i=0;
+	while(i<5) 
 	{
 		printf("%d ",a[i]);
-		i--;
-	}
-	printf("\n");
+		
+		i++;
+	}	
+	  printf("\n");
 }
