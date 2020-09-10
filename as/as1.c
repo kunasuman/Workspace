a307564776e0ec6a6978c59ff9822879e808ce15
kunasuman <kunasuman666@gmail.com>
2020-09-10 22:32:56 +0530

    array with string

---
+++ as/as1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+void main()
+{
+	char a[20];
+
+	printf("enter the your name\n");
+	scanf("%s",a);
+
+	printf("helo %s,welcome\n %s,%s,\n",a,a,a);
+
+}
---
+++ as/as3.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+void main()
+{
+	char a[30];
+
+	printf("enter the name:\n");
+	scanf("%s,%s",a,a);
+	
+	
+	printf(" name is %s %s",a,a);
+}
